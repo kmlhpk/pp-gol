c6eacc2f461df0200ab0cfde3eddefa88662ba19
Kamil <kmlhpk@users.noreply.github.com>
2020-02-17 00:43:47 +0000

    implemented death border rule correctly

--- onefile-gol.c
+++ onefile-gol.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 
-#define UNIVERSE_HARDCODE {{{'.','.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.','.'}},5,9,4,8,0}
+#define UNIVERSE_HARDCODE {{{'.','.','.','.','.','.','.','.','.'},{'.','.','.','*','.','.','.','.','.'},{'.','.','*','*','*','.','.','.','.'},{'.','.','.','*','.','.','.','.','.'},{'.','.','.','.','.','.','.','.','.'}},5,9,4,8,0}
 
 struct universe {
   char cells[5][9];
@@ -30,27 +31,31 @@ void write_out_console (struct universe *u) {
 };
 
 int is_alive(struct universe *u, int column, int row){
+  //printf("I receive %d as my row and %d as my column \n",row,column);
   if (u->cells[row][column] == '.') {
-    printf("%d,%d is dead \n",row,column);
+    //printf("%d,%d is dead \n",row,column);
     return(0);
   }
   else {
-    printf("%d,%d is alive \n",row,column);
+    //printf("%d,%d is alive \n",row,column);
     return(1);
   }
 };
 
 int will_be_alive(struct universe *u, int column, int row){
+
+  //printf("investigating %d,%d's neighbours \n",row,column);
+
   int north, northeast, east, southeast, south, southwest, west, northwest = 0;
 
   if (row == 0) {
    north = 0;
   } else {
-    north = is_alive(u,column,row+1);}
+    north = is_alive(u,column,row-1);}
   if (row == 0 || column == u->MAX_COL_INDEX) {
     northeast = 0;
   } else {
-    northeast = is_alive(u,column+1,row+1);}
+    northeast = is_alive(u,column+1,row-1);}
   if (column == u->MAX_COL_INDEX) {
     east = 0;
   } else {
@@ -58,15 +63,15 @@ int will_be_alive(struct universe *u, int column, int row){
   if (row == u->MAX_ROW_INDEX || column == u->MAX_COL_INDEX) {
     southeast = 0;
   } else {
-    southeast = is_alive(u,column+1,row-1);}
+    southeast = is_alive(u,column+1,row+1);}
   if (row == u->MAX_ROW_INDEX) {
     south = 0;
   } else {
-    south = is_alive(u,column,row-1);}
+    south = is_alive(u,column,row+1);}
   if (row == u->MAX_ROW_INDEX || column == 0) {
     southwest = 0;
   } else {
-    southwest = is_alive(u,column-1,row-1);}
+    southwest = is_alive(u,column-1,row+1);}
   if (column == 0) {
     west = 0;
   } else {
@@ -74,20 +79,24 @@ int will_be_alive(struct universe *u, int column, int row){
   if (row == 0 ||column == 0) {
     northwest = 0;
   } else {
-    northwest = is_alive(u,column-1,row+1);}
+    northwest = is_alive(u,column-1,row-1);}
 
   int neighbours = northwest+north+northeast+west+southwest+south+southeast+east;
-  printf("%d",neighbours);
+  //printf("I have %d alive neighbours \n",neighbours);
 
   if (neighbours == 3) {
+    //printf("so I live \n");
     return(1);
   } else if (neighbours == 2) {
-    if (is_alive(u,row,column)) {
+    if (is_alive(u,column,row)) {
+      //printf("and I'm alive, so I live \n");
       return(1);
     } else {
+      //printf("and I'm dead, so I die \n");
       return(0);
     }
   } else {
+    //printf("so I die \n");
     return(0);
   }
 };
@@ -105,6 +114,7 @@ void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int 
       }
     }
   }
+
   // replace the following with pointer magic
   for (int i = 0; i < u->rows; i++) {
     for (int j = 0; j < u->cols; j++) {
@@ -121,6 +131,14 @@ void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int 
 
 int main(){
   struct universe v = UNIVERSE_HARDCODE;
+  write_out_console(&v);
+  evolve(&v,will_be_alive);
+  write_out_console(&v);
+  evolve(&v,will_be_alive);
+  write_out_console(&v);
+  evolve(&v,will_be_alive);
+  write_out_console(&v);
+  evolve(&v,will_be_alive);
   write_out_console(&v);
   evolve(&v,will_be_alive);
   write_out_console(&v);
