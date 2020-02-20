74d6ab911c4bda8e4ca93e481241f3d192dc1760
Kamil Hepak <kmlhpk@users.noreply.github.com>
2020-02-20 01:01:38 +0000

    finished library functionality

--- onefile-gol.c
+++ onefile-gol.c
@@ -1,14 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define UNIVERSE_HARDCODE {{{'.','.','.','.','.','.','.','.','.'},{'.','.','.','*','.','.','.','.','.'},{'.','.','*','*','*','.','.','.','.'},{'.','.','.','*','.','.','.','.','.'},{'.','.','.','.','.','.','.','.','.'}},5,9,4,8,3,1.0/15.0,3,1,1.0/15.0}
-
-// #define STUPID_1 {{{"."}},1,1,0,0,1}
-
-// #define STUPID_2 {{{"*","*"},{".","."}},2,2,1,1,1}
-
 struct universe {
-  char cells[5][9];
+  char **cells;
   int rows;
   int cols;
   int maxRowIndex;
@@ -20,24 +14,39 @@ struct universe {
   float aliveAverageFrac;
 };
 
-void read_in_file(FILE *infile, struct universe *u) {
+void read_in_file(FILE *infile, struct universe *u){
+  // Checks if the file is empty
   if (infile == NULL){
     fprintf(stderr, "Empty file\n");
     exit(1);
   }
-  char buffer[513];
-  char grid[513];
   int columns = 0;
-  int rows = 1;
+  int rows = 0;
+  // Allocates slightly more than maximum column amount's worth of memory to a line "buffer"
+  char *line = (char*)malloc(513 * sizeof(char));
+  if (line == NULL){
+    fprintf(stderr, "Failed to allocate memory\n");
+    exit(1);
+  }
+  // Allocates one character pointer's worth of memory to the pointer-based "2D array"
+  char **grid = (char **)malloc(sizeof(char *));
+  if (grid == NULL){
+    fprintf(stderr, "Failed to allocate memory\n");
+    exit(1);
+  }
   char c;
   int currentColumns;
+  // Loops through characters in the input until it sees an EOF
   while (1) {
     c = fgetc(infile);
+    // On the first line, sets the number of columns for each new line to check against
     if (columns == 0) {
       if (c == EOF) {break;} else {
+        // Loops through characters in the line until it sees a line-feed
         while (c != '\n') {
           if (c == '.' || c == '*') {
-            buffer[columns] = c;
+            // Adds the character to the line buffer
+            line[columns] = c;
             columns += 1;
           } else {
             fprintf(stderr, "Ill-formatted input - expected only . or *, saw a different character.\n");
@@ -48,9 +57,11 @@ void read_in_file(FILE *infile, struct universe *u) {
       }
     } else {
       if (c == EOF) {break;} else {
+        // Loops through characters in the line until it sees a line-feed
         while (c != '\n') {
           if (c == '.' || c == '*') {
-            buffer[currentColumns] = c;
+            // Adds the character to the line buffer
+            line[currentColumns] = c;
             currentColumns += 1;
           } else {
             fprintf(stderr, "Ill-formatted input - expected only . or * (and line-feed), saw a different character.\n");
@@ -60,28 +71,51 @@ void read_in_file(FILE *infile, struct universe *u) {
         }
       }
     }
-    if (rows > 1) {
+    // After each line starting with the second, checks if column numbers are equal
+    if (rows > 0) {
       if (currentColumns != columns) {
         fprintf(stderr, "Ill-formatted input - column numbers aren't equal in every row\n");
         exit(1);
       }
     }
+    // Adds line buffer to array of lines
+    grid[rows] = line;
     currentColumns = 0;
     rows += 1;
+    // Adds one more line's worth of space in the 2D array
+    grid = (char **)realloc(grid, (rows+1) * sizeof(char *));
+    // Makes the line buffer point at some fresh blank memory
+    line = (char *)malloc(513 * sizeof(char));
   }
 
-  // If we can definitely expect all valid inputs, whether from a file or from stdio, to have a trailing LF (\n) before EOF, then this rows -= 1 makes sense. Otherwise, it fucks us over a bit.
-  rows -= 1;
-
-  printf("%d columns! \n",columns);
-  printf("%d rows! \n",rows);
   if (columns == 0 || columns > 512) {
     fprintf(stderr, "Invalid column number \n");
     exit(1);
   }
+
+  // Populates the universe struct with necessary data
+  u->cells = grid;
+  u->rows = rows;
+  u ->cols = columns;
+  u->maxRowIndex = rows-1;
+  u->maxColIndex = columns-1;
+  int alive = 0;
+  for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++) {
+      if (grid[i][j] == '*'){
+        alive += 1;
+      }
+    }
+  }
+  u->aliveNow = alive;
+  u->aliveSoFar = alive;
+  u->aliveNowFrac = (float)alive / (float)(rows*columns);
+  u->aliveAverageFrac = (float)alive / (float)(rows*columns);
+  u->generations = 1;
 };
 
 void write_out_file(FILE *outfile, struct universe *u) {
+  // Writes out every character in the universe's cell board
   for (int i = 0; i < u->rows; i++){
     for (int j = 0; j < u->cols; j++) {
       fputc(u->cells[i][j], outfile);
@@ -105,22 +139,22 @@ int mod (int a, int b){
 }
 
 int is_alive(struct universe *u, int column, int row){
-  #define ISALIVE_DEBUG1 printf("I receive %d as my row and %d as my column \n",row,column);
+  // Returns 0 if cell is dead, 1 if cell is alive, error if cell is somehow an illegal character
   if (u->cells[row][column] == '.') {
-    #define ISALIVE_DEAD printf("%d,%d is dead \n",row,column);
     return(0);
-  }
-  else {
-    #define ISALIVE_ALIVE printf("%d,%d is alive \n",row,column);
+  } else if (u->cells[row][column] == '*') {
     return(1);
+  } else {
+    fprintf(stderr, "Encountered illegal character in cell grid");
+    exit(1);
   }
 };
 
 int will_be_alive(struct universe *u, int column, int row){
-  #define WILLBEALIVE_DEBUG1 printf("investigating %d,%d's neighbours \n",row,column);
-
+  // Initialises all neighbours as dead
   int north, northeast, east, southeast, south, southwest, west, northwest = 0;
 
+  // Checks if all 8 of the cell's neighbours are alive, unless the neighbour is outside the cell grid, in which case it is dead
   if (row == 0) {
    north = 0;
   } else {
@@ -155,29 +189,26 @@ int will_be_alive(struct universe *u, int column, int row){
     northwest = is_alive(u,column-1,row-1);}
 
   int neighbours = northwest+north+northeast+west+southwest+south+southeast+east;
-  
-  #define WILLBEALIVE_DEBUG2 printf("I have %d alive neighbours \n",neighbours);
 
+  // Applies standard game of life rule to determine if the cell will be alive
   if (neighbours == 3) {
-    #define SOILIVE printf("so I live \n");
     return(1);
   } else if (neighbours == 2) {
     if (is_alive(u,column,row)) {
-      #define ALIVE_LIVE printf("and I'm alive, so I live \n");
       return(1);
     } else {
-      #define DEAD_DIE printf("and I'm dead, so I die \n");
       return(0);
     }
   } else {
-    #define SOIDIE printf("so I die \n");
     return(0);
   }
 };
 
 int will_be_alive_torus(struct universe *u,  int column, int row){
+  // Initialises all neighbours as dead
   int north, northeast, east, southeast, south, southwest, west, northwest = 0;
 
+  // Checks if all 8 of the cell's neighbours are alive; if the neighbour is "outside" the cell grid, then it is actually on the other side of the grid corresponding to a toroidal topology
   north = is_alive(u,mod(column,u->cols),mod(row-1,u->rows));
   northeast = is_alive(u,mod(column+1,u->cols),mod(row-1,u->rows));
   east = is_alive(u,mod(column+1,u->cols),mod(row,u->rows));
@@ -189,6 +220,7 @@ int will_be_alive_torus(struct universe *u,  int column, int row){
 
   int neighbours = northwest+north+northeast+west+southwest+south+southeast+east;
 
+  // Applies standard game of life rule to determine if the cell will be alive
   if (neighbours == 3) {
     return(1);
   } else if (neighbours == 2) {
@@ -204,7 +236,9 @@ int will_be_alive_torus(struct universe *u,  int column, int row){
 
 void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int row)){
   int aliveNew = 0;
-  char newCells[u->rows][u->cols]; // MALLOC THIS LATER!
+  // Makes new generation's cell grid
+  char newCells[u->rows][u->cols];
+  // Populates new cell grid 
   for (int i = 0; i < u->rows; i++) {
     for (int j = 0; j < u->cols; j++) {
       if (rule(u, j, i)) {
@@ -216,16 +250,15 @@ void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int 
     }
   }
 
-  printf("%d alive in this generation\n",u->aliveNow);
-  printf("%d alive so far\n",u->aliveSoFar);
-
+  // Sets correct information for new generation, calculates statistics
   u->aliveNow = aliveNew;
   u->aliveNowFrac = (float)u->aliveNow / (float)(u->rows * u->cols);
   u->aliveSoFar += u->aliveNow;
   u->generations += 1;
   u->aliveAverageFrac = (float)u->aliveSoFar / (float)(u->generations*u->rows*u->cols);
 
-  // replace the following with pointer magic
+  // Replaces the current universe's cell grid with the new cell grid
+  // replace the following with pointer magic????????????????????
   for (int i = 0; i < u->rows; i++) {
     for (int j = 0; j < u->cols; j++) {
       u->cells[i][j] = newCells[i][j];
@@ -234,9 +267,11 @@ void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int 
 };
 
 void print_statistics(struct universe *u){
+  // Prints statistics about the universe (funnily enough)
   printf("%.3f%% of cells currently alive\n",u->aliveNowFrac*(float)100);
   printf("%.3f%% of cells alive on average\n",u->aliveAverageFrac*(float)100);
 };
+
 /*
 int main(){
   struct universe v = UNIVERSE_HARDCODE;
@@ -256,8 +291,15 @@ return 0;
 */
 
 int main(){
-  struct universe v = UNIVERSE_HARDCODE;
-  read_in_file(stdin, &v);
+  struct universe v; 
+  read_in_file(stdin,&v);
+  evolve(&v,will_be_alive);
+  //evolve(&v,will_be_alive);
+  //evolve(&v,will_be_alive);
+  //evolve(&v,will_be_alive);
+  //evolve(&v,will_be_alive);
+  write_out_file(stdout,&v);
+  print_statistics(&v);
 }
 
 /* Vanilla
